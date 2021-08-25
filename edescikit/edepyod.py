18d5037461ad3c3c2541305a30fcfd2dddbf4890
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2021-08-25 17:56:55 +0300

    added initial pyod methods

---
+++ edescikit/edepyod.py
@@ -0,0 +1,44 @@
+"""
+Copyright 2021, Institute e-Austria, Timisoara, Romania
+    http://www.ieat.ro/
+Developers:
+ * Gabriel Iuhasz, iuhasz.gabriel@info.uvt.ro
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at:
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import pyod
+from pyod.models.abod import ABOD
+from pyod.models.cblof import CBLOF
+from pyod.models.feature_bagging import FeatureBagging
+from pyod.models.hbos import HBOS
+from pyod.models.iforest import IForest
+from pyod.models.knn import KNN
+from pyod.models.lof import LOF
+from pyod.models.mcd import MCD
+from pyod.models.ocsvm import OCSVM
+from pyod.models.lscp import LSCP
+
+
+def ede_abod(contamination):
+    clf = ABOD(contamination=contamination)
+    return clf
+
+
+def ede_cblof(contamination,
+              check_estimator,
+              random_state):
+
+    clf = CBLOF(contamination=contamination,
+                check_estimator=check_estimator,
+                random_state=random_state)
+    return clf
+
